c057ebae39ba09c1d5129688474531b6f042670a
SatriaReza <74123282+SatriaReza@users.noreply.github.com>
2022-10-24 15:05:55 +0700

    Update irs.php
    LIttle bit update

--- irs.php
+++ irs.php
@@ -1,3 +1,16 @@
+<?php
+require('db/db_login.php');
+
+if (isset($_POST['submit'])) {
+    $semester = $_POST['semester'];
+    $sksKum = $_POST['sksKum'];
+    $scanIRS = $_POST['scanIRS'];
+
+    $query = "INSERT INTO irs (semester_mhs, sks, berkas_irs) VALUES ('$semester','$sksKum', '$scanIRS')";
+    $query_run = mysqli_query($con, $query);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,7 +21,9 @@
     <title>IRS</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="nav/nav_mhs/style_nav.css" />
+    <link rel="stylesheet" href="css/upload-file.css">
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
 
 </head>
 
@@ -26,45 +41,45 @@
             </div>
             <div class="sidenav card">
                 <ul class="grid gap-2">
-                    <li><a id="Profil" href="../ir"><img src="nav/icon/IRS.png" alt="" /> Profil</a></li>
+                    <li><a id="Profil" href="#"><img src="nav/icon/IRS.png" alt="" /> Profil</a></li>
                     <hr>
                     <li><a id="IRS" href="#"><img src="nav/icon/IRS.png" alt="" /> Data IRS</a></li>
                     <hr>
-                    <li><a id="KHS" href="#"><img src="nav/icon/KHS.png" alt="" /> Data KHS</a></li>
+                    <li><a id="KHS" href="khs.php"><img src="nav/icon/KHS.png" alt="" /> Data KHS</a></li>
                     <hr>
-                    <li><a id="PKL" href="#"><img src="nav/icon/PKL.png" alt="" /> Data PKL</a></li>
+                    <li><a id="PKL" href="pkl.php"><img src="nav/icon/PKL.png" alt="" /> Data PKL</a></li>
                     <hr>
-                    <li><a id="Skripsi" href="#"><img src="nav/icon/Skripsi.png" alt="" /> Data Skripsi</a></li>
+                    <li><a id="Skripsi" href="skripsi.php"><img src="nav/icon/Skripsi.png" alt="" /> Data Skripsi</a></li>
                     <hr>
                     <li><a id="Logout" href="#"><img src="nav/icon/Logout.png" alt="" /> Keluar</a></li>
                 </ul>
             </div>
         </div>
-        <div class="content-irs card sky-blue-50 grow" style="padding:50px 70px; margin-top: 10vh; margin-bottom:10vh; margin-right:5vw;">
+        <div class="card sky-blue-50 grow" style="padding:50px 70px; margin-top: 10vh; margin-bottom:10vh; margin-right:5vw;">
             <div class="card-header mb-2" style="font-size: 30px ;font-weight: 700;">Data IRS</div>
             <div class="card-body">
-                <form method="POST" action="" class="grid gap-7">
-                    <div class="from-group mt-3">
+                <form method="POST" name="fIRS" action="" class="grid dropzone" onsubmit="return validateForm()">
+                    <div class="from-group mt-3 mb-7">
                         <label class="block tracking-wide  text-sm font-bold mb-2" for="grid-state">
                             Semester Aktif
                         </label>
                         <div class="relative">
-                            <select name="semester" class="block appearance-none w-full bg-gray-100 border border-gray-200 text-gray-500 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state" required>
+                            <select name="semester" class="block appearance-none w-full bg-gray-100 border border-gray-200 text-gray-500 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
                                 <option value="">Pilih Semester</option>
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                                <option value="6">6</option>
-                                <option value="7">7</option>
-                                <option value="8">8</option>
-                                <option value="9">9</option>
-                                <option value="10">10</option>
-                                <option value="11">11</option>
-                                <option value="12">12</option>
-                                <option value="13">13</option>
-                                <option value="14">14</option>
+                                <option value=1>1</option>
+                                <option value=2>2</option>
+                                <option value=3>3</option>
+                                <option value=4>4</option>
+                                <option value=5>5</option>
+                                <option value=6>6</option>
+                                <option value=7>7</option>
+                                <option value=8>8</option>
+                                <option value=9>9</option>
+                                <option value=10>10</option>
+                                <option value=11>11</option>
+                                <option value=12>12</option>
+                                <option value=13>13</option>
+                                <option value=14>14</option>
                             </select>
                             <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
@@ -73,37 +88,64 @@
                             </div>
                         </div>
                     </div>
-                    <div class="form-group">
-                        <label class="block tracking-wide text-gray-700 text-sm font-bold mb-2" for="grid-city">
+                    <div class="form-group mb-7">
+                        <label class="block tracking-wide text-gray-700 text-sm font-bold mb-2" for="">
                             SKS Kumulatif
                         </label>
-                        <input name="SKS Kumatif" class="appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="number" placeholder="Masukkan SKS Kumulatif">
+                        <input name="sksKum" class="appearance-none block w-full bg-gray-100 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="number" placeholder="Masukkan SKS Kumulatif">
                     </div>
-                    <div class="form-group">
-                        <label class="block tracking-wide text-gray-700 text-sm font-bold mb-2" for="grid-city">
-                            Upload Scan KHS
-                        </label>
-                        <div class="flex justify-center items-center w-full">
-                            <label for="dropzone-file" class="flex flex-col justify-center items-center w-full h-64 bg-gray-100 rounded-lg border-2 border-gray-100 border-solid cursor-pointer dark:hover:bg-gray-50 dark:bg-gray-100 hover:bg-gray-200 dark:border-gray-100 dark:hover:border-gray-200 dark:hover:bg-gray-200">
-                                <div class="flex flex-col justify-center items-center pt-5 pb-6">
-                                    <svg aria-hidden="true" class="mb-3 w-10 h-10 text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path>
-                                    </svg>
-                                    <p class="mb-2 text-sm text-gray-600 dark:text-gray-600"><span class="font-semibold">Click to upload</span> or drag and drop</p>
-                                </div>
-                                <input id="dropzone-file" type="file" class="hidden" />
-                            </label>
+                    <label class="block tracking-wide text-gray-700 text-sm font-bold mb-2" for="">
+                        Upload Scan IRS
+                    </label>
+                    <div class="upload-irs ">
+                        <div class="mb-7 upload-file bg-gray-200">
+                            <input class="file-input" id="inIRS" type="file" accept=".pdf" name="scanIRS" style="display:none;">
+                            <span id="icon-up"><i class="fas fa-cloud-upload-alt text-gray-700"></i></span>
+                            <span id="uploaded-file" class="block tracking-wide text-gray-700 text-sm font-bold opacity-75">Upload Scan IRS</span>
                         </div>
                     </div>
-                    <div class="justify-self-end">
-                        <button class="bg-violet-500 hover:bg-violet-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" type="submit">Save</button>
+                    <div class="flex justify-between text-gray-700 shadow-inner rounded p-3 bg-red-400 mb-7" id="ferror" style="display: none;">
+                        <p class="self-center text-slate-100"><strong><i class="fas fa-info-circle"></i> Mohon untuk mengisi semua data.</strong></p>
+                    </div>
+                    <div class="grid justify-self-end">
+                        <button class="bg-violet-500 hover:bg-violet-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" type="submit" name="submit">Save</button>
                     </div>
                 </form>
             </div>
         </div>
     </div>
+
     <script>
         document.getElementById('IRS').style.opacity = '2';
+
+        function validateForm() {
+            var semester = document.forms["fIRS"]["semester"].value;
+            var sksKum = document.forms["fIRS"]["sksKum"].value;
+            var IRS = document.getElementById("IRS");
+            if (semester == "" || sksKum == '' || IRS.files.length === 0) {
+                document.getElementById('ferror').style.display = "flex";
+                return false;
+            }
+        }
+
+        const formUp = document.querySelector(".upload-file"),
+            fileInput = document.querySelector(".file-input")
+
+        formUp.addEventListener("click", () => {
+            fileInput.click();
+        });
+
+        let inIRS = document.getElementById("inIRS");
+        let uploadedFile = document.getElementById("uploaded-file");
+        let iconUP = document.getElementById("icon-up");
+
+
+        inIRS.addEventListener("change", ()=>{
+            let inputFile = document.querySelector("input[type=file]").files[0];
+
+            uploadedFile.innerText = inputFile.name;
+            iconUP.innerHTML = '<i class="fas fa-file-alt text-gray-700"></i>';
+        })
     </script>
 </body>
 
