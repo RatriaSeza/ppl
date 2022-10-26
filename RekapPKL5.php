ec0c44ab43ccb7c503a6449a43cdd3db98006717
RakeWangsa <116138780+RakeWangsa@users.noreply.github.com>
2022-10-27 04:45:16 +0700

    Add files via upload

--- RekapPKL5.php
+++ RekapPKL5.php
@@ -4,7 +4,7 @@
 		<meta charset="UTF-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<title>Rekap Skripsi</title>
+		<title>Rekap PKL</title>
 
         <!-- Tailwind -->
 		<script src="https://cdn.tailwindcss.com"></script>
@@ -47,7 +47,7 @@
             </div>
         </div>
         <div class="col-span-2">
-        <?php
+            <?php
                 $servername = "localhost";
                 $username = "root";
                 $password = "";
@@ -60,28 +60,27 @@
                     die("Connection failed: " . $conn->connect_error);
                 }
 
-                $skripsi17 = "SELECT * FROM mahasiswa INNER JOIN skripsi ON mahasiswa.NIM = skripsi.nim_mhs WHERE skripsi.status_skripsi = 'Sudah' AND mahasiswa.angkatan='17';";
-                $result17 = $conn->query($skripsi17);
-                $skripsi18 = "SELECT * FROM mahasiswa INNER JOIN skripsi ON mahasiswa.NIM = skripsi.nim_mhs WHERE skripsi.status_skripsi = 'Sudah' AND mahasiswa.angkatan='18';";
-                $result18 = $conn->query($skripsi18);
-                $skripsi19 = "SELECT * FROM mahasiswa INNER JOIN skripsi ON mahasiswa.NIM = skripsi.nim_mhs WHERE skripsi.status_skripsi = 'Sudah' AND mahasiswa.angkatan='19';";
-                $result19 = $conn->query($skripsi19);
-                $skripsi20 = "SELECT * FROM mahasiswa INNER JOIN skripsi ON mahasiswa.NIM = skripsi.nim_mhs WHERE skripsi.status_skripsi = 'Sudah' AND mahasiswa.angkatan='20';";
-                $result20 = $conn->query($skripsi20);
-
+                $pkl17 = "SELECT mahasiswa.NIM, mahasiswa.nama_mhs, pkl.status_pkl FROM mahasiswa INNER JOIN pkl ON mahasiswa.NIM = pkl.nim_mhs WHERE pkl.status_pkl='Sudah' AND mahasiswa.angkatan='17';";
+                $result17 = $conn->query($pkl17);
+                $pkl18 = "SELECT mahasiswa.NIM, mahasiswa.nama_mhs, pkl.status_pkl FROM mahasiswa INNER JOIN pkl ON mahasiswa.NIM = pkl.nim_mhs WHERE pkl.status_pkl='Sudah' AND mahasiswa.angkatan='18';";
+                $result18 = $conn->query($pkl18);
+                $pkl19 = "SELECT mahasiswa.NIM, mahasiswa.nama_mhs, pkl.status_pkl FROM mahasiswa INNER JOIN pkl ON mahasiswa.NIM = pkl.nim_mhs WHERE pkl.status_pkl='Sudah' AND mahasiswa.angkatan='19';";
+                $result19 = $conn->query($pkl19);
+                $pkl20 = "SELECT mahasiswa.NIM, mahasiswa.nama_mhs, pkl.status_pkl FROM mahasiswa INNER JOIN pkl ON mahasiswa.NIM = pkl.nim_mhs WHERE pkl.status_pkl='Sudah' AND mahasiswa.angkatan='20';";
+                $result20 = $conn->query($pkl20);
 
                 $count1=mysqli_num_rows($result17);
                 $count2=mysqli_num_rows($result18);
                 $count3=mysqli_num_rows($result19);
                 $count4=mysqli_num_rows($result20);
 
-                $blm17 = "SELECT mahasiswa.NIM, mahasiswa.nama_mhs, skripsi.status_skripsi FROM mahasiswa INNER JOIN skripsi ON mahasiswa.NIM = skripsi.nim_mhs WHERE skripsi.status_skripsi='Belum' AND mahasiswa.angkatan='17';";
+                $blm17 = "SELECT mahasiswa.NIM, mahasiswa.nama_mhs, pkl.status_pkl FROM mahasiswa INNER JOIN pkl ON mahasiswa.NIM = pkl.nim_mhs WHERE pkl.status_pkl='Belum' AND mahasiswa.angkatan='17';";
                 $hasil17 = $conn->query($blm17);
-                $blm18 = "SELECT mahasiswa.NIM, mahasiswa.nama_mhs, skripsi.status_skripsi FROM mahasiswa INNER JOIN skripsi ON mahasiswa.NIM = skripsi.nim_mhs WHERE skripsi.status_skripsi='Belum' AND mahasiswa.angkatan='18';";
+                $blm18 = "SELECT mahasiswa.NIM, mahasiswa.nama_mhs, pkl.status_pkl FROM mahasiswa INNER JOIN pkl ON mahasiswa.NIM = pkl.nim_mhs WHERE pkl.status_pkl='Belum' AND mahasiswa.angkatan='18';";
                 $hasil18 = $conn->query($blm18);
-                $blm19 = "SELECT mahasiswa.NIM, mahasiswa.nama_mhs, skripsi.status_skripsi FROM mahasiswa INNER JOIN skripsi ON mahasiswa.NIM = skripsi.nim_mhs WHERE skripsi.status_skripsi='Belum' AND mahasiswa.angkatan='19';";
+                $blm19 = "SELECT mahasiswa.NIM, mahasiswa.nama_mhs, pkl.status_pkl FROM mahasiswa INNER JOIN pkl ON mahasiswa.NIM = pkl.nim_mhs WHERE pkl.status_pkl='Belum' AND mahasiswa.angkatan='19';";
                 $hasil19 = $conn->query($blm19);
-                $blm20 = "SELECT mahasiswa.NIM, mahasiswa.nama_mhs, skripsi.status_skripsi FROM mahasiswa INNER JOIN skripsi ON mahasiswa.NIM = skripsi.nim_mhs WHERE skripsi.status_skripsi='Belum' AND mahasiswa.angkatan='20';";
+                $blm20 = "SELECT mahasiswa.NIM, mahasiswa.nama_mhs, pkl.status_pkl FROM mahasiswa INNER JOIN pkl ON mahasiswa.NIM = pkl.nim_mhs WHERE pkl.status_pkl='Belum' AND mahasiswa.angkatan='20';";
                 $hasil20 = $conn->query($blm20);
 
                 $hitung1=mysqli_num_rows($hasil17);
@@ -93,7 +92,7 @@
             ?>
 
 <div style="margin-left:5%;margin-right:5%;padding-top:100px">
-<strong>Rekap Mahasiswa Skripsi</strong>
+<strong>Rekap Mahasiswa PKL</strong>
         <canvas id="StackedbarChartcanvas" style="width:80%;height:200px;margin-top:20px;"></canvas>
     </div>
     
@@ -107,14 +106,14 @@
             ],
             datasets: [
                 {
-                    label: "Mahasiswa Lulus Skripsi",
+                    label: "Mahasiswa Lulus PKL",
                     backgroundColor: "rgba(191, 0, 255, 0.7)",
                     borderColor: "rgba(191, 0, 255, 1)",
                     borderWidth: 1,
                     data: [<?php echo $count1; ?>, <?php echo $count2; ?>, <?php echo $count3; ?>, <?php echo $count4; ?>]
                 },
                 {
-                    label: "Mahasiswa Belum Skripsi",
+                    label: "Mahasiswa Belum PKL",
                     backgroundColor: "blue",
                     borderColor: "blue",
                     borderWidth: 1,
@@ -155,7 +154,7 @@
 
     </script>
         <div style="margin-top:25px;margin-left:50px">
-<a href="listskripsi5.php" class="bg-purple-500 hover:bg-purple-400 text-white font-bold py-2 px-4 border-b-4 border-purple-700 hover:border-purple-500 rounded">
+<a href="listpkl5.php" class="bg-purple-500 hover:bg-purple-400 text-white font-bold py-2 px-4 border-b-4 border-purple-700 hover:border-purple-500 rounded">
 List
 </a>
 </div>
