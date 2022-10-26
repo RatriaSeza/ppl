01832fdfbbc48e7ed426949656f133ea6408d169
SatriaReza <74123282+SatriaReza@users.noreply.github.com>
2022-10-26 18:51:04 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,77 +1,72 @@
 <?php
-    session_start();
-    require_once('db/db_login.php');
+session_start();
+require_once('db/db_login.php');
 
-    if (isset($_POST['submit'])) {
-        $email = $_POST['email'];
-        $password = md5($_POST['password']);
+if (isset($_POST['submit'])) {
+    $email = $_POST['email'];
+    $password = md5($_POST['password']);
 
-        if ($password == '' || $email == '') {
-            $error = '<div class="alert alert-danger">Masukkan Email dan Password!</div>';
+    if ($password == '' || $email == '') {
+        $error = '<div class="alert alert-danger">Masukkan Email dan Password!</div>';
+    } else {
+        $query = " SELECT * FROM user WHERE email='" . $email . "' AND password='" . $password . "' ";
+
+        $result = mysqli_query($con, $query);
+        if (!$result) {
+            die("Could not query the database <br />" . mysqli_error($con));
         } else {
-            $query = " SELECT * FROM user WHERE email='" . $email . "' AND password='" . $password . "' ";
-
-            $result = mysqli_query($con, $query);
-            if (!$result) {
-                die("Could not query the database <br />" . mysqli_error($con));
+            if (mysqli_num_rows($result) > 0) {
+                $_SESSION['email'] = $email;
+                header('Location: edit_profil.php');
+                exit;
             } else {
-                if (mysqli_num_rows($result) > 0) {
-                    $_SESSION['email'] = $email;
-                    header('Location: edit_profil.php');
-                    exit;
-                } else {
-                    $error = '<div class="alert alert-danger">Email atau Password yang dimasukkan salah</div>';
-                }
+                $error = '<div class="alert alert-danger">Email atau Password yang dimasukkan salah</div>';
             }
-            mysqli_close($con);
         }
+        mysqli_close($con);
     }
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-        <style>
-            body {
-                background: #f8f9fa;
-            }
-        </style>
-        <title>Login</title>
-    </head>
-    <body>
-        <div class="container mt-5">
-            <div class="row">
-                <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-                    <div class="card border-0 shadow rounded-3 my-5">
-                        <div class="card-body p-4 p-sm-5">
-                            <h3 class="card-title text-center mb-5 fw-bold">LOGIN</h3>
-                            <form method="POST" >
-                                <div class="form-floating mb-3">
-                                    <input type="email" class="form-control" id="floatingInput" name="email" placeholder="Email">
-                                    <label for="floatingInput" class="opacity-75">Email</label>
-                                </div>
-                                <div class="form-floating mb-3">
-                                    <input type="password" class="form-control" id="floatingPassword" name="password" placeholder="Password">
-                                    <label for="floatingPassword" class="opacity-75">Password</label>
-                                </div>
-                                <div class="error p-2">
-                                    <?php if (isset($error)) echo $error ?>
-                                </div>
-                                <div class="d-grid">
-                                    <button class="btn btn-primary btn-login fw-normal" name="submit" type="submit">Login</button>
-                                </div>
-                                <div class="register mt-3">
-                                    Don't have an Account? <a href="register.php">Register Now</a>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <style>
+        body {
+            background: #f8f9fa;
+        }
+    </style>
+    <title>Login</title>
+</head>
+
+<body>
+    <div class="grid mx-96 flex" style="margin-top: 30vh;">
+        <form class="flex flex-col grid card w-full ">
+            <label class="block text-gray-500 font-bold mb-0 pr-4" for="inline-full-name">
+                NIM/NIP/Email
+            </label>
+            <div class="mb-4">
+                <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="Jane Doe">
             </div>
-        </div>
-    </body>
+            <label class="block text-gray-500 font-bold mb-0 pr-4" for="inline-password">
+                Password
+            </label>
+            <div class="mb-4">
+                <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" placeholder="******************">
+            </div>
+            <div class="justify-self-end">
+                <button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+                    Login
+                </button>
+            </div>
+        </form>
+    </div>
+</body>
+
 </html>
