6f38462c13a112de4610ce31690808d15b5ffb7b
SatriaReza <74123282+SatriaReza@users.noreply.github.com>
2022-10-26 11:18:26 +0700

    Update khs.php

--- khs.php
+++ khs.php
@@ -69,12 +69,13 @@ if (isset($_POST['submit'])) {
             </div>
             <div class="sidenav card">
                 <ul id="navlist" class="divide-y divide-gray-500 grid">
+                    <li><a id="Dashboard" href="DashboardMahasiswa.php"><i class="fas fa-house"></i> Dashboard</a></li>
                     <li><a id="Profil" href=""><i class="fas fa-user"></i> Profil</a></li>
-                    <li><a id="IRS" href="#"><i class="fas fa-file-lines"></i> Data IRS</a></li>
+                    <li><a id="IRS" href="irs.php"><i class="fas fa-file-lines"></i> Data IRS</a></li>
                     <li><a id="KHS" href="#"><i class="fas fa-file-lines"></i> Data KHS</a></li>
                     <li><a id="PKL" href="#"><i class="fas fa-building"></i> Data PKL</a></li>
                     <li><a id="Skripsi" href="#"><i class="fas fa-book-bookmark"></i> Data Skripsi</a></li>
-                    <li><a id="Logout" href="#"><i class="fas fa-right-from-bracket"></i> Keluar</a></li>
+                    <li><a id="Logout" href="logout.php"><i class="fas fa-right-from-bracket"></i> Keluar</a></li>
                 </ul>
             </div>
         </div>
