470cca9558578a1e7e371c98310d95fb7972657a
AndiraFaqih <108270542+AndiraFaqih@users.noreply.github.com>
2022-10-27 10:30:09 +0700

    Add files via upload

--- login.php
+++ login.php
@@ -1,26 +1,40 @@
 <?php
 session_start();
-require_once('db/db_login.php');
+require_once('db_con.php');
 
 if (isset($_POST['submit'])) {
-    $email = $_POST['email'];
-    $password = md5($_POST['password']);
+    $username = $_POST['username'];
+    $password = ($_POST['password']);
 
-    if ($password == '' || $email == '') {
-        $error = '<div class="alert alert-danger">Masukkan Email dan Password!</div>';
+    if ($password == '' || $username == '') {
+        $error = '<div class="alert alert-danger">Masukkan username dan Password!</div>';
     } else {
-        $query = " SELECT * FROM user WHERE email='" . $email . "' AND password='" . $password . "' ";
+        $query = 'SELECT * FROM user WHERE username="' . $username . '" AND password="' .$password. '"';
 
         $result = mysqli_query($con, $query);
         if (!$result) {
             die("Could not query the database <br />" . mysqli_error($con));
         } else {
             if (mysqli_num_rows($result) > 0) {
-                $_SESSION['email'] = $email;
-                header('Location: edit_profil.php');
+                $row = mysqli_fetch_assoc($result); 
+                $role = $row["role"];
+                $_SESSION['username'] = $username;
+                if ($role == 'mahasiswa'){
+                    header('Location: DashboardMahasiswa.php'); 
+                }
+                else if ($role == 'dosen'){
+                    header('Location: dashdosen6.php');
+                }
+                else if ($role == 'operator'){
+                    header('Location: DashboardOperator.php');
+                }
+                else if ($role == 'departemen'){
+                    header('Location: dashdepartemen5.php');
+                }
+                $_SESSION['role'] = $role; 
                 exit;
             } else {
-                $error = '<div class="alert alert-danger">Email atau Password yang dimasukkan salah</div>';
+                $error = '<div class="alert alert-danger">Username atau Password yang dimasukkan salah</div>';
             }
         }
         mysqli_close($con);
@@ -46,22 +60,29 @@ if (isset($_POST['submit'])) {
 </head>
 
 <body>
+    <?php if (isset($tes)) echo $tes ?>
     <div class="grid mx-96 flex" style="margin-top: 30vh;">
-        <form class="flex flex-col grid card w-full ">
-            <label class="block text-gray-500 font-bold mb-0 pr-4" for="inline-full-name">
-                NIM/NIP/Email
-            </label>
-            <div class="mb-4">
-                <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="Jane Doe">
+        <form class="mx-auto flex flex-col grid card w-96 p-10" method="post">
+            <div class="text-center text-5xl font-medium mb-7">
+                Login
             </div>
-            <label class="block text-gray-500 font-bold mb-0 pr-4" for="inline-password">
+            <label class="block text-gray-500 text-xl font-bold mb-0 pr-4" for="inline-full-name">
+                Username
+            </label>
+            <div class="mb-5">
+                <input name="username" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" placeholder="NIM / NIP / Email">
+            </div>
+            <label class="block text-gray-500 text-xl font-bold mb-0 pr-4" for="inline-password">
                 Password
             </label>
-            <div class="mb-4">
-                <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" placeholder="******************">
+            <div class="mb-5">
+                <input name="password" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" placeholder="password">
+            </div>
+            <div>
+                <?php if (isset($error)) echo $error ?>
             </div>
             <div class="justify-self-end">
-                <button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+                <button name="submit" class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
                     Login
                 </button>
             </div>
